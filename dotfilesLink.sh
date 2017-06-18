2ba0ca386c0b97be7491031519945976e8e030b4
tetsuya <air@macbookair.local>
2017-06-18 15:11:23 +0900

    first commit

---
+++ dotfilesLink.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.vim ~/.vim
+
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.zsh_history ~/.zsh_history
+
